a3c2a339b7b300a07a3ce8d14226a3956db993f5
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2026-05-19 01:56:24 +0530

    fix: replace generated sitemap with static sitemap

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import generateSitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    generateSitemap({
-      hostname: 'https://chefmate-frontend.vercel.app'
-    })
-  ],
+  plugins: [react()],
 })
